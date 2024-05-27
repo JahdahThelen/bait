529ae18dbfa27f13f1676ddaf3610fb4e12e0760
Jahdah Thelen <jahdah.thelen@gmail.com>
2024-05-27 16:41:09 +0200

    fixate package versions and add error handling

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from typing import Annotated
 
 import pandas
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, HTTPException
 
 
 app = FastAPI()
 
 @app.post("/convert_xlsx/")
-async def create_upload_file(file: UploadFile):
-    excel_data_df = pandas.read_excel(file.file)
-    json_str = excel_data_df.to_json(orient='records')
-    return json_str
+async def convert_excel_to_json(file: UploadFile):
+    try:
+        excel_data_df = pandas.read_excel(file.file)
+        json_str = excel_data_df.to_json(orient='records')
+        return json_str
+    except:
+        raise HTTPException(status_code=422, detail="Conversion was not succesfull")
